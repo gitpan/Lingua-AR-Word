45e6635a06913e0008fe496e6e04c7b61492266b
Andrea Benazzo <benazzo@cpan.org>
2005-06-23 10:40:52 +0000

    Import of BENAZZO/Lingua-AR-Word-1.4 from CPAN.
    gitpan-cpan-distribution: Lingua-AR-Word
    gitpan-cpan-version:      1.4
    gitpan-cpan-path:         BENAZZO/Lingua-AR-Word-1.4.tar.gz
    gitpan-cpan-author:       BENAZZO
    gitpan-cpan-maturity:     released

---
+++ lib/Lingua/AR/Word.pm
@@ -0,0 +1,119 @@
+package Lingua::AR::Word;
+
+use 5.008006;
+use strict;
+use warnings;
+use utf8;
+
+use Lingua::AR::Word::Stem;	#needed to find the stem
+use Lingua::AR::Db;		#needed to get the translation
+use Switch;
+
+
+our @ISA = qw();
+
+our $VERSION = '1.4';
+
+
+sub new{
+
+	my $this={};
+	$this->{WORD}=$_[1];
+	$this->{STEM}=&stem($this->{WORD});
+	$this->{TRANSLATION}="";
+	$this->{ARABTEX}=&encode($this->{WORD});
+
+	bless($this);
+return $this;
+}
+
+
+
+sub get_word{
+
+	my $this=shift;
+return "WORD: $this->{WORD}\n";
+}
+
+sub get_stem{
+
+	my $this=shift;
+return "STEM: $this->{STEM}\n";
+
+}
+
+sub get_arabtex{
+
+	my $this=shift;
+return "ARABTEX: $this->{ARABTEX}\n";
+
+}
+
+sub get_translation{
+
+	my $this=shift;
+	$this->{TRANSLATION}=Lingua::AR::Db::translate($this->{WORD},$_[0]);
+return "TRANSLATION: $this->{TRANSLATION}\n";
+
+}
+
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+Lingua::AR::Word - Perl extension for getting the stem and the translation of Arabic words
+
+=head1 SYNOPSIS
+
+use utf8;
+
+my $word=Lingua::AR::Word->new(ARABIC_WORD_IN_UTF8);
+
+
+open FOUTPUT, ">>TEST" or die "Cannot create TEST: $!\n";
+binmode(FOUTPUT,":utf8");
+
+print FOUTPUT $word->get_word();
+print FOUTPUT $word->get_stem();
+print FOUTPUT $word->get_arabtex();
+
+my $db=Lingua::AR::Db->new("./dicts","en");
+print FOUTPUT $word->get_translation($db);
+
+close FOUTPUT;
+
+
+=head1 DESCRIPTION
+
+In order to work on an Arabic word, you need to create the object Dict::Word, passing the Arabic word encoded in utf8 to the constructor.
+You will then be able to get the stem through get_stem().
+You will get the ArabTeX translittered form through get_arabtex().
+If you also want the translation, you'll need to create the $db object (described in the Dict::Db module), so that it may look for its meaning in the correct Database.
+
+Remember that input-output directly to shell will not be useful as long as your shell doesn't support utf8 encoded characters.
+That's because I piped the output to another file, forcing its writing in utf8.
+
+
+
+=head1 SEE ALSO
+
+You may find more info about ArabTeX encoding at ftp://ftp.informatik.uni-stuttgart.de/pub/arabtex/arabtex.htm
+
+
+
+=head1 AUTHOR
+
+Andrea Benazzo, E<lt>andy@slacky.itE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2005 Andrea Benazzo. All rights reserved.
+ This program is free software; you can redistribute it and/or
+ modify it under the same terms as Perl itself.
+
+
+=cut
---
+++ lib/Lingua/AR/Word/Stem.pm
@@ -0,0 +1,129 @@
+package Lingua::AR::Word;
+
+use strict;
+use utf8;
+use Switch;
+
+
+sub stem{
+
+	my $stem;
+	my $word=$_[0];
+	
+	#let's strip down every prefix and suffix I'm aware of
+	if($word=~/^([وفب]*ال|[بيلمتوسن]*ت|[بلوكف]*م|[ال]*ل|[ولسف]*ي|[وفلب]*ا|)(.*?)(ات|وا|تا|ون|وه|ان|تي|ته|تم|كم|ه[نم]*|ها|ية|تك|نا|ي[نه]*|[ةهيا]|)$/)
+	{
+ 		$word=$2;
+       }
+
+       #let's strip down all other unnecessary letters
+       my $length=length($word);
+
+       switch($length){
+			case 3	{
+				$stem=$word;
+			}
+			case 4	{
+				$stem=&four($word);
+			}
+			case 5	{
+				$stem=&five($word);
+			}
+			case 6	{
+				$stem=&six($word);
+			}
+			else	{
+				$stem="NotFound";
+			}
+
+       }
+return $stem;
+}
+
+sub four{
+	my $word=$_[0];
+	
+	if($word=~/(.)(.)(ا|ي|و)(.)/){
+		$word=$1.$2.$4;
+	}
+	elsif ($word=~/(.)(ا|و|ط|ي)(.)(.)/){
+		$word=$1.$3.$4;
+	}
+	else{
+		$word="NotFound";
+	}
+}
+
+sub five{
+	my $word=$_[0];
+	
+	if($word=~/(.)(.)(ا)(ا)(.)/){
+		$word=$1.$2.$5;
+	}
+	elsif ($word=~/(.)(ت|ي)(.)(ا)(.)/){
+		$word=$1.$3.$5;
+	}
+	elsif ($word=~/(.)(و)(ا)(.)(.)/){
+		$word=$1.$4.$5;
+	}
+	elsif ($word=~/(.)(ا)(.)(ي|و)(.)/){
+		$word=$1.$3.$5;
+	}
+	elsif ($word=~/(.)(.)(.)(ا|ي|و)(.)/){
+		$word=$1.$2.$3.$5;
+		$word=&four($word);
+	}
+	elsif ($word=~/(.)(.)(ا|ي)(.)(.)/){
+		$word=$1.$2.$4.$5;
+		$word=&four($word);
+	}
+	else{
+		$word="NotFound";
+	}
+}
+
+sub six{
+	my $word=$_[0];
+	
+	if($word=~/(.)(و)(ا)(.)(ي)(.)/){
+		$word=$1.$4.$6;
+	}
+	elsif ($word=~/(.)(.)(ا)(.)(ي)(.)/){
+		$word=$1.$2.$4.$6;
+		$word=&four($word);
+	}
+	else{
+		$word="NotFound";
+	}
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Lingua::AR::Word::Stem - Perl extension for finding the stem of a given Arabic word
+
+=head1 SYNOPSIS
+
+  use Lingua::AR::Word::Stem;
+
+$stem=Lingua::AR::Word::stem(ARABIC_WORD_IN_UTF8);
+
+=head1 DESCRIPTION
+
+This module will take care of finding the stem of an Arabic word, through chopping the prefixes and suffixes of the word and by taking away unnecessary letters in the middle of the word.
+
+
+=head1 AUTHOR
+
+Andrea Benazzo, E<lt>andy@slacky.itE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2005 Andrea Benazzo. All rights reserved.
+ This program is free software; you can redistribute it and/or
+ modify it under the same terms as Perl itself.
+
+
+=cut
